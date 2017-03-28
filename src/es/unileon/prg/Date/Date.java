6e975bb73d6ab5284540b8a83d9396a679eef7b4
csanta02 <csanta02@estudiantes.unileon.com>
2017-03-28 14:14:59 +0200

    Primer commit

---
+++ src/es/unileon/prg/Date/Date.java
@@ -0,0 +1,50 @@
+public class Date{
+
+//Definir las variables.
+private int day;
+private int month;
+private int year;
+
+//Constructor.
+public Date(int _day, int _month, int _year){
+
+_day=day;
+_month=month;
+_year=year;
+
+}
+
+//Métodos get.
+public int getDay(){
+		return _day;
+	}
+
+public int getMonth(){
+		return _month;
+	}
+
+public int getYear(){
+		return _year;
+	}
+
+//Métodos set.
+
+
+//Crear los métodos isSame utilizando if. 
+public String isSameYear(){ 
+if()
+}
+
+public String isSameMonth(){ 
+if()
+}
+
+public String isSameDay(){ 
+if()
+}
+
+public String isSame(){ 
+if()
+}
+
+}
