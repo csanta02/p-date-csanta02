12bc899b07049b451f11d40fc7ac0e956d663749
csanta02 <csanta02@estudiantes.unileon.es>
2017-04-02 17:43:06 +0200

    if isSame

--- src/es/unileon/prg/Date/Date.java
+++ src/es/unileon/prg/Date/Date.java
@@ -8,10 +8,10 @@ private int year;
 //Constructor.
 public Date(int _day, int _month, int _year) throws DateException{
 
-this.day=day;
-if(month > 0 && month <= 12) {this.month=month;}
+this.day=_day;
+if(month > 0 && month <= 12) {this.month=_month;}
 else{throw new DateException ("Mes" + month + "no valido." + "\nValores posibles entre 1 y 2");}
-this.year=year;
+this.year=_year;
 
 }
 
@@ -34,23 +34,32 @@ public int getYear(){
 
 
 //Crear los métodos isSame utilizando if. 
-public boolean isSameYear(Date today, Date yesterday){ 
-
-if(this.year.compareTo(another.getYear())==true){
-System.out.println("Es el mismo año");
-}else{System.out.println("No es el mismo año");}
+public boolean isSameYear(Date another){ 
+	if(this.year == another.getYear()){System.out.println("Es el mismo año.\n");
+	return this.year == another.getYear();}
+	else{System.out.println("No es el mismo año.\n");
+	return this.year != another.getYear();}
 }
 
-/*public String isSameMonth(){ 
-if()
+public boolean isSameMonth(Date another){ 
+	if(this.month == another.getMonth()){System.out.println("Es el mismo mes.\n");
+	return this.month == another.getMonth();}
+	else{System.out.println("No es el mismo mes.\n");
+	return this.month != another.getMonth();}
 }
 
-public String isSameDay(){ 
-if()
+public boolean isSameDay(Date another){ 
+	if(this.day == another.getDay()){System.out.println("Es el mismo día.\n");
+	return this.day == another.getDay();}
+	else{System.out.println("No es el mismo día.\n");
+	return this.day != another.getDay();}
 }
 
-public String isSame(){ 
-if()
+public boolean isSame(Date another){ 
+	if(this.day == another.getDay() && this.month == another.getMonth() && this.year == another.getYear()){System.out.println("Es la misma fecha.\n");
+	return this.day == another.getDay() && this.month == another.getMonth() && this.year == another.getYear();}
+	else{System.out.println("No es la misma fecha.\n");
+	return this.day != another.getDay() || this.month != another.getMonth() || this.year != another.getYear();}
 }
-*/
+
 }
